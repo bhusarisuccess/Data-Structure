9726ae42060ac9d232e6a96e72b36113dce0df76
Abhay Bhusari <bhusari.success@gmail.com>
2024-03-03 17:22:49 +0530

    Update the code changes

--- src/BubbleSort/SortArray.java
+++ src/BubbleSort/SortArray.java
@@ -14,11 +14,15 @@ public class SortArray {
         for (int i = 0; i < arr.length; i++) {
             for (int j = 1; j < arr.length - i; j++) {
                 if (arr[j] < arr[j - 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j - 1];
-                    arr[j - 1] = temp;
+                    swapElement(arr, j, j - 1);
                 }
             }
         }
+    }
+
+    private static void swapElement(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
     }
 }
